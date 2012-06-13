d5d2b6dfa5a47c0991ebc00a740865384ef3e671
stong70@yahoo.com <stong70@yahoo.com>
2012-06-12 22:34:30 -0700

    stong

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4dbc12d3a37219a27fd8009c6547bc5e7a8daf3ea7a45a06ed8e02cfe1e7761b9f3a1501a463345271c9e9d2cc014d471d177bb09491ae46b326e670e53b2682'
